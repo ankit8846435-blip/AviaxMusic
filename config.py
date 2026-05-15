5c7821c27510bc9796e66351502820cc2f9fdbff
ankit8846435-blip <ankit8846435@gmail.com>
2026-05-15 18:53:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ class Config:
             url for url in getenv("COOKIES_URL", "").split(" ")
             if url and "batbin.me" in url
         ]
-        self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
+        self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://repgyetdcodkynrbxocg.supabase.co/storage/v1/object/public/images/telegram-1778831823743-da6ecd4e.jpg")
         self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
         self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
 
